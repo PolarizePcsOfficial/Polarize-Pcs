a558dee5bd0107aa342b924a6b37c957c8bda995
PolarizePcsOfficial <123495627+PolarizePcsOfficial@users.noreply.github.com>
2024-04-17 19:02:23 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+// THIS IS FOR THE PRE BUILT SECTION!!!!!!!!!!!!!!!!
+
+const PRE = document.getElementById('NO-PRE');
+
+PRE.addEventListener('click', function(event) {
+  event.preventDefault(); 
+  window.alert('There is nothing here just yet! :)');
+});
+
+
+
